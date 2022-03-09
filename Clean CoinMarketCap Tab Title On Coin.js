382909591842caad7a16ad0fbbf8c56d3193807a
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-08 21:44:49 -0800

    Update Clean CoinMarketCap Tab Title On Coin.js

--- Clean CoinMarketCap Tab Title On Coin.js
+++ Clean CoinMarketCap Tab Title On Coin.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name        Clean CoinMarketCap Tab Title On Coin
 // @version     1.01
-// @match       http://coinmarketcap.com/currencies/*/
-// @match       https://coinmarketcap.com/currencies/*/
+// @match       http://coinmarketcap.com/*
+// @match       https://coinmarketcap.com/*
 // @description Shorten CoinMarketCap's tab title
 // ==/UserScript==
 
 var title = document.title;
 if(document.URL.indexOf("coinmarketcap.com/currencies/") >= 0)
 {
-  var [remainder1, lastPart] = title.split('|');
-  var [remainder2, secondLastPart] = remainder1.split(' to ');
-  var [firstPart, midPart] = remainder2.split(' price today, ');
-  document.title = firstPart + ' ' + midPart + ' -' + lastPart;
+    var [remainder1, lastPart] = title.split('|');
+    var [remainder2, secondLastPart] = remainder1.split(' to ');
+    var [firstPart, midPart] = remainder2.split(' price today, ');
+    document.title = firstPart + ' ' + midPart + ' -' + lastPart;
 }
